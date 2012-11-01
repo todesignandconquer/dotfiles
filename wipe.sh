bd1be4982ab0306bcbcaf5cfa8a9b523e11856fb
Eli Anderson <elianderson03@gmail.com>
2012-10-31 18:04:41 -0700

    cleaning up files

--- wipe.sh
+++ wipe.sh
@@ -2,5 +2,5 @@ git pull
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "elianderson.zsh-theme" --exclude "wipe.sh" --exclude "README.md" -av . ~
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "elianderson.zsh-theme" --exclude "wipe.sh" --exclude "README.md" -av ./dotfiles/* ~
 fi
